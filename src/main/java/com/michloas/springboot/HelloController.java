eb28a6cf1b513f7f6447a8005f0d8c4eb06d035f
michloas <601045301@qq.com>
2018-08-08 16:09:13 +0800

    #初始化项目

---
+++ src/main/java/com/michloas/springboot/HelloController.java
@@ -0,0 +1,16 @@
+package com.michloas.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by michloas on 2018/8/8.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello spring boot 2.0.4  1233444111";
+    }
+
+}
---
+++ src/main/java/com/michloas/springboot/TestController.java
@@ -0,0 +1,10 @@
+package com.michloas.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by michloas on 2018/8/8.
+ */
+@RestController
+public class TestController {
+}
